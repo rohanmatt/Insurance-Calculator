11fbc57a320309e7765e30808c134c10ec09cd3b
Rohan <rohanroymathew01@gmail.com>
2021-05-15 12:51:14 +0530

    Add project files.

---
+++ WindowsApplication17/Form17.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(30, 108)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(139, 100)
+        Me.ListBox1.TabIndex = 0
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(381, 108)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(140, 24)
+        Me.ComboBox1.TabIndex = 2
+        Me.ComboBox1.Text = "Age"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(381, 170)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(80, 20)
+        Me.CheckBox1.TabIndex = 3
+        Me.CheckBox1.Text = "Smoking"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(381, 218)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 27)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(381, 300)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(87, 27)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(381, 370)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 16)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Label1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(51, 305)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(159, 22)
+        Me.TextBox1.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DimGray
+        Me.ClientSize = New System.Drawing.Size(633, 483)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Name = "Form1"
+        Me.Text = "Insurance"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ WindowsApplication17/Form17.vb
@@ -0,0 +1,55 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListBox1.Items.Add("Male")
+        ListBox1.Items.Add("Female")
+        ComboBox1.Items.Add("30 to <40")
+        ComboBox1.Items.Add("40 to <50")
+        ComboBox1.Items.Add("50 to <60")
+        ComboBox1.Items.Add(">60")
+        Label1.Text = Date.Today
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If ListBox1.SelectedItem = "Male" And ComboBox1.SelectedItem = "30 to <40" And CheckBox1.Checked = "False" Then
+            TextBox1.Text = "Insurance is 40K"
+        ElseIf ListBox1.SelectedItem = "Male" And ComboBox1.SelectedItem = "30 to <40" And CheckBox1.Checked = "True" Then
+            TextBox1.Text = "Insurance is 30K"
+        ElseIf ListBox1.SelectedItem = "Female" And ComboBox1.SelectedItem = "30 to <40" And CheckBox1.Checked = "False" Then
+            TextBox1.Text = "Insurance is 60K"
+        ElseIf ListBox1.SelectedItem = "Female" And ComboBox1.SelectedItem = "30 to <40" And CheckBox1.Checked = "True" Then
+            TextBox1.Text = "Insurance is 50K"
+        ElseIf ListBox1.SelectedItem = "Male" And ComboBox1.SelectedItem = "40 to <50" And CheckBox1.Checked = "False" Then
+            TextBox1.Text = "Insurance is 60K"
+        ElseIf ListBox1.SelectedItem = "Male" And ComboBox1.SelectedItem = "40 to <50" And CheckBox1.Checked = "True" Then
+            TextBox1.Text = "Insurance is 40K"
+        ElseIf ListBox1.SelectedItem = "Female" And ComboBox1.SelectedItem = "40 to <50" And CheckBox1.Checked = "False" Then
+            TextBox1.Text = "Insurance is 50K"
+        ElseIf ListBox1.SelectedItem = "Female" And ComboBox1.SelectedItem = "40 to <50" And CheckBox1.Checked = "True" Then
+            TextBox1.Text = "Insurance is 20K"
+        ElseIf ListBox1.SelectedItem = "Male" And ComboBox1.SelectedItem = "50 to <60" And CheckBox1.Checked = "False" Then
+            TextBox1.Text = "Insurance is 70K"
+        ElseIf ListBox1.SelectedItem = "Male" And ComboBox1.SelectedItem = "50 to <60" And CheckBox1.Checked = "True" Then
+            TextBox1.Text = "Insurance is 50K"
+        ElseIf ListBox1.SelectedItem = "Female" And ComboBox1.SelectedItem = "50 to <60" And CheckBox1.Checked = "False" Then
+            TextBox1.Text = "Insurance is 40K"
+        ElseIf ListBox1.SelectedItem = "Female" And ComboBox1.SelectedItem = "50 to <60" And CheckBox1.Checked = "True" Then
+            TextBox1.Text = "Insurance is 20K"
+        ElseIf ListBox1.SelectedItem = "Male" And ComboBox1.SelectedItem = ">60" And CheckBox1.Checked = "False" Then
+            TextBox1.Text = "Insurance is 80K"
+        ElseIf ListBox1.SelectedItem = "Male" And ComboBox1.SelectedItem = ">60" And CheckBox1.Checked = "True" Then
+            TextBox1.Text = "Isurance is 40K"
+        ElseIf ListBox1.SelectedItem = "Female" And ComboBox1.SelectedItem = ">60" And CheckBox1.Checked = "False" Then
+            TextBox1.Text = "Insurance is 40K"
+        ElseIf ListBox1.SelectedItem = "Female" And ComboBox1.SelectedItem = ">60" And CheckBox1.Checked = "True" Then
+            TextBox1.Text = "Insurance is 40K"
+        End If
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Application.Exit()
+
+    End Sub
+End Class
